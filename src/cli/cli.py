9d61b6f4c095fce18b1a87cc8a3977e3264a946d
SleepyWoodpecker <shawnweichew@gmail.com>
2025-08-23 10:42:22 +0800

    feat: added some basic error handling for input

--- src/cli/cli.py
+++ src/cli/cli.py
@@ -3,6 +3,7 @@ from textual.app import App, ComposeResult
 from textual.widgets import Footer, Header, Input, Label
 from textual.containers import HorizontalGroup, VerticalGroup, Container
 from textual.validation import Number
+from textual.binding import Binding
 
 
 class AutoCalCli(App):
@@ -59,6 +60,8 @@ class CurrentCalibrationProgressIndicator(VerticalGroup):
 class CurrentCalibrationUserInputWidget(VerticalGroup):
     """The widget which accepts user input"""
 
+    BINDINGS = [("escape", "blur", "unfocus input")]
+
     def compose(self) -> ComposeResult:
         with Container(id="current-calibration-input-container"):
             yield Label("Current pressure", id="current-pressure-label")
@@ -71,17 +74,28 @@ class CurrentCalibrationUserInputWidget(VerticalGroup):
 
     @on(Input.Submitted)
     def accept_user_input(self, event: Input.Submitted):
-        input_widget = self.query_one(Input)
-        error_label = self.query_one("#error-message", Label)
+        input_widget = self.query_one("#current-pressure-input", Input)
         # ensure that the input is a valid number
         if event.validation_result:
             if event.validation_result.is_valid:
-                self.mount(Label(input_widget.value))
-                error_label.update("")
+                self.set_error_label("")
             else:
-                error_label.update("Pressure should be a number!")
+                self.set_error_label("Pressure should be a number!")
 
         input_widget.value = ""
+
+    def set_error_label(self, error_value: str) -> None:
+        error_label = self.query_one("#error-message", Label)
+        error_label.update(error_value)
+
+    def on_mount(self) -> None:
+        self.call_after_refresh(lambda: self.screen.set_focus(None))
+
+    def action_blur(self) -> None:
+        self.screen.set_focus(None)
+        self.set_error_label("")
+        input_widget = self.query_one("#current-pressure-input", Input)
+        input_widget.remove_class("-invalid")
 
 
 class PreviousCalculationDisplay(VerticalGroup):
